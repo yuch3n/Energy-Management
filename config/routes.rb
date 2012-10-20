0fe5149bfee66e34fe332e47b408f72534679180
Leslie Chang <hi.lesliechang@gmail.com>
2012-10-20 00:32:03 -0700

    added feedback view

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ EnegeryManagement::Application.routes.draw do
 
   resources :admin, :only => [:index]
   resources :tips, :only => [:index]
-
+  resources :feedback, :only => [:index]
   namespace :admin do
     resources :tips
     resources :categories do
