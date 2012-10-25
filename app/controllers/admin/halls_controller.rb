d5bb902f7ff66c682d42bdd53fb1803b98d6474e
saasbook <saasbook@saasbook.(none)>
2012-10-25 00:48:40 -0700

    add building stuff

--- app/controllers/admin/halls_controller.rb
+++ app/controllers/admin/halls_controller.rb
@@ -25,7 +25,7 @@ class Admin::HallsController < ApplicationController
   end
 
   def create
-    @hall = Hall.create(:name => params[:hall.name], :streamid => params[:hall.streamid])
+    @hall = Hall.create params[:hall]
     if @hall && !params[:streamid].nil? && !params[:hall].nil?
       flash[:notice] = "#{@hall.name} was successfully created."
       redirect_to admin_halls_path
