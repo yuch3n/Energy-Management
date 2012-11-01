67d7cebfc4e2e2724cf171a1763fd845fda4d932
Daniel Choi <danieljchoi92@gmail.com>
2012-10-31 21:04:37 -0700

    more sendgrid configs

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 EnegeryManagement::Application.initialize!
-EnegeryManagement::Application.configure do
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.sendgrid.com",
-  :port                 => '587',
-  :domain               => 'heroku.com',
-  :user_name      => "app8427599@heroku.com",
-  :password       => "ulhuqeff",
-  :authentication       => 'plain',
-}
-end
