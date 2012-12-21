df2772b4c7775004c42221ccdadd9d4b2055d106
Daniel Choi <danieljchoi92@gmail.com>
2012-12-21 11:55:28 -0800

    email recipient changed

--- app/controllers/emailer_controller.rb
+++ app/controllers/emailer_controller.rb
@@ -6,7 +6,7 @@ class EmailerController < ApplicationController
 
 
   def submit_feedback
-      recipient = 'danieljchoi92@gmail.com'
+      recipient = 'myPower@berkeley.edu'
       subject = 'myPower Feedback'
       message = params[:message]
       if not message.empty? 
@@ -19,7 +19,7 @@ class EmailerController < ApplicationController
   end
 
   def submit_flag
-      recipient = 'danieljchoi92@gmail.com'
+      recipient = 'myPower@berkeley.edu'
       subject = 'myPower Energy Use Anomaly Reported'
       message = "A user has submitted an energy use anomaly flag for building: "+Hall.find_by_id(Integer(params[:id])).name
       Emailer.contact(recipient, subject, message).deliver
